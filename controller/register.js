f5ab21d0ed23a840b2ba7c0b7ae52a5880670eaf
Abdul Mohaiman <abdulmohaiman333@gmail.com>
2025-08-03 22:30:40 -0700

    update controller

--- controller/register.js
+++ controller/register.js
@@ -1,81 +1,21 @@
-// const signup_page = async(req, reply)=>{
-//     return reply.sendFile('/register.html')
-// };
-
-
-// const signupPost = async(req, reply)=>{
-//     try {
-//         const { name, email, password } = req.body;
-//         const hashPass = await bcrypt.hash(password, 10);
-//         const credentials = await Signup.create({name, email, password: hashPass});
-//         return reply.send(credentials);
-//     } catch (error) {
-//         console.error('internal server error.')
-//     }
-// };
-
-
-// const loginForm = async(req, reply)=>{
-//     return reply.sendFile('/login.html')
-// };
-
-
-
-// const loginFormGet = async(req, reply)=>{
-//     try {
-//         const {email, password} = req.body;
-//         const credentials = await Signup.findOne({email});
-
-//         if(!credentials){
-//             console.log('email is not found.');
-//             return reply.status(404).send({error: 'Email not found.'})
-//         }
-
-//         const hashPassGet = bcrypt.compare(password, credentials.password);
-//         if(!hashPassGet){
-//             console.log('invalid credentials.');
-//             return reply.status(500).send({error: 'Invalid password.'})
-//         }
-
-//         return reply.redirect('/home.html');
-//     } catch (error) {
-//         console.log('internal server error.')
-//         return reply.status(500).send({error: 'Internal server error.'})
-//     }
-// };
-
-
-// const home = async(req, reply)=>{
-//     return reply.sendFile('/home.html')
-// }
-
-
-// module.exports = { signup_page, signupPost, loginForm, loginFormGet, home };
-
-
-
-
 const path = require('path');
 const Signup = require('../models/register.js');
 const bcrypt = require('bcrypt');
 
-// Show signup form (GET /signup)
 const signup_page = async (req, reply) => {
   return reply.sendFile('/register.html');
 };
 
-// Handle signup form submit (POST /signup)
+
 const signupPost = async (req, reply) => {
   try {
     const { name, email, password } = req.body;
 
-    // Hash password
     const hashPass = await bcrypt.hash(password, 10);
 
-    // Save to DB
+
     const credentials = await Signup.create({ name, email, password: hashPass });
 
-    // Redirect to home page on success
     return reply.redirect('/home.html');
   } catch (error) {
     console.error('Signup Error:', error);
@@ -83,12 +23,10 @@ const signupPost = async (req, reply) => {
   }
 };
 
-// Show login form (GET /loginForm)
 const loginForm = async (req, reply) => {
   return reply.sendFile('/login.html');
 };
 
-// Handle login form submit (POST /loginFormGet)
 const loginFormGet = async (req, reply) => {
   try {
     const { email, password } = req.body;
@@ -114,7 +52,7 @@ const loginFormGet = async (req, reply) => {
   }
 };
 
-// Serve home page
+
 const home = async (req, reply) => {
   return reply.sendFile('/home.html');
 };
